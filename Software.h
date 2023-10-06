2d47ddd07ea2290a7d3441bcbcb9e8a06761301d
v1chi <130424809+v1chi@users.noreply.github.com>
2023-10-06 04:26:17 -0300

    a

--- Software.h
+++ Software.h
@@ -10,7 +10,7 @@ class Software
     string name;
     string developer;
     bool underage;
-    vector<User*> users;
+    //vector<User*> users;
     int price;
 
     public:
@@ -31,11 +31,11 @@ Software::Software(string name, string developer, int price)
     this->name=name;
     this->developer=developer;
     this->price=price;
-    vector<User*> users;
+    //vector<User*> users;
     
 };
 void Software::setAgeClassification(bool underage){this->underage=underage;};
 string Software::getName(){return this->name;};
 string Software::getDeveloper(){return this->developer;};
 int Software::getPrice(){return this->price;};
-vector<User*> Software::getUsers(){return this->users;};
+//vector<User*> Software::getUsers(){return this->users;};
